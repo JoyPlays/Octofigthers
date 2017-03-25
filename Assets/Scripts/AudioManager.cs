690aac5e125c01d88a90942d7f870cc8780ed748
Davis <davis.toliasvili@gmail.com>
2017-03-25 12:40:39 +0300

    Push

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+
+	public AudioSource backgroundMusic;
+	public AudioSource ambientShore;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	public void StopMusic()
+	{
+		ambientShore.Stop();
+	}
+
+}
---
+++ Assets/Scripts/MenuLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLogic : MonoBehaviour
+{
+	public AudioManager audioManager;
+	// Update is called once per frame
+
+	public void StartGame()
+	{
+		audioManager.StopMusic();
+		SceneManager.LoadScene("WorldMap");
+	}
+
+	public void ReturnToMainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+}
