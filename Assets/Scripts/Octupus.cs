8200bd33818c74c8708d586d8cd90d469258e6de
Rolands <rolands.cipa@gmail.com>
2017-03-25 10:20:45 +0200

    Added first scripts and scene update

---
+++ Assets/Scripts/Octupus.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Octupus : MonoBehaviour 
+{
+    public float AttackTimeMin;
+    public float AttackTimeMax;
+    public float Damage;
+    public int LegCount;
+    Time AttackTimer;
+
+	void Start ()
+    {
+        //AttackTimer = Random.Range(AttackTimeMin, AttackTimeMax);//max exclusive
+    }
+	
+	void Update ()
+    {
+        //Random.Range(AttackTimeMin, AttackTimeMax);//max exclusive
+    }
+
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Helath;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
