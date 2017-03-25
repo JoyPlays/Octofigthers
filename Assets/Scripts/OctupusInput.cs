6fa9fe78343049028e791b435240c80586035a5f
Rolands <rolands.cipa@gmail.com>
2017-03-25 13:23:46 +0300

    input fix :D

---
+++ Assets/Scripts/OctupusInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctupusInput : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed left click.");
+        }
+
+
+    }
+}
